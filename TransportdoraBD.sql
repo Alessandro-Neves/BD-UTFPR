a1a0c18164b0c7ebc86e9c52cd88c7e5df7820de
Alessandro-Neves <76932488+Alessandro-Neves@users.noreply.github.com>
2021-08-23 02:09:07 -0300

    add initial config
    as drop schema, create schema and use schema

--- TransportdoraBD.sql
+++ TransportdoraBD.sql
@@ -1,28 +1,55 @@
+-- Alessandro Neves dos Santos, 2044323
+DROP SCHEMA IF EXISTS TRANSPORTADORA;
+CREATE SCHEMA TRANSPORTADORA;
+USE TRANSPORTADORA;
+
+DROP TABLE IF EXISTS
+    FUNCIONARIOS,
+    OPERARIOS,
+    FUNCIONARIOS_ADMINISTRATIVOS,
+    MOTORISTAS,
+    GERENTES,
+    CONTRATOS_TRABALHO,
+    UNIDADES_TRANSPORTADORA,
+    ENDERECO_TRANSPORTADORA,
+    CATEGORIAS_VEICULO,
+    CATEGORIAS_CARGA,
+    TIPO_DE_VEICULO,
+    VEICULO_PERTENCE,
+    VEICULOS,
+    VIAJENS_ENTREGA,
+    LOTES_ENTREGA,
+    DESTINOS,
+    PRODUTOS,
+    PREPARA_LOTE,
+    CLIENTES,
+    ENDERECO_CLIENTE;
+
 -- Funcionários
 CREATE TABLE FUNCIONARIOS(
     id INTEGER PRIMARY,
     cpf INTEGER,
     nome VARCHAR(50),
     UNIQUE(cpf)
-)
+);
 CREATE TABLE OPERARIOS(
     id INTEGER PRIMARY KEY,
     FOREIGN KEY (id) REFERENCES FUNCIONARIOS(id)
-)
+);
 CREATE TABLE FUNCIONARIOS_ADMINISTRATIVOS(
     id INTEGER PRIMARY KEY,
     FOREIGN KEY (id) REFERENCES FUNCIONARIOS(id)
-)
+);
 CREATE TABLE MOTORISTAS(
     id INTEGER PRIMARY KEY,
     cnh INTEGER,
     UNIQUE(cnh),
     FOREIGN KEY (id) REFERENCES FUNCIONARIOS(id)
-)
+);
 CREATE TABLE GERENTES(
     id INTEGER PRIMARY KEY,
     FOREIGN KEY (id) REFERENCES FUNCIONARIOS(id)
-)
+);
 -- Contratos de trabalho
 CREATE TABLE CONTRATOS_TRABALHO(
     id INTEGER PRIMARY KEY,
@@ -33,7 +60,7 @@ CREATE TABLE CONTRATOS_TRABALHO(
     idUnidadeTransportadora INTEGER,
     FOREIGN KEY (idFuncionario) REFERENCES FUNCIONARIOS(id), -- Relação: Funcionarios <Possui> ContratosTrabalho
     FOREIGN KEY (idUnidadeTransportadora) REFERENCES UNIDADES_TRANSPORTADORA(id) -- Relação: UnidadesTransportadora <possui> Contratos de Trabalho
-)
+);
 -- Unidade Transportadora
 
 CREATE TABLE UNIDADES_TRANSPORTADORA(
@@ -41,7 +68,7 @@ CREATE TABLE UNIDADES_TRANSPORTADORA(
     idGerente INTEGER NOT NULL,
     UNIQUE(idGerente),
     FOREIGN KEY (idGerente) REFERENCES GERENTES(id) -- Relacão: Gerente <responsavel> UnidadeTransportadora
-)
+);
 
 -- Endereço Unidade Transportadora
 
@@ -52,20 +79,20 @@ CREATE TABLE ENDERECO_TRANSPORTADORA(
     cidade VARCHAR(20),
     estado VAR(2),
     FOREIGN KEY (id) REFERENCES UNIDADES_TRANSPORTADORA(id)
-)
+);
 
 -- Categorias de Veiculo
 CREATE TABLE CATEGORIAS_VEICULO(
     id INTEGER PRIMARY KEY,
     nome VARCHAR(20),
     descricao VARCHAR(200)
-)
+);
 -- Categorias de carga
 CREATE TABLE CATEGORIAS_CARGA(
     id INTEGER PRIMARY KEY,
     classe VAR(1),
     peso_maximo INTEGER 
-)
+);
 -- Possui: relação entre Categoris de Carga e Categorias de Veiculo
 
 CREATE TABLE TIPO_DE_VEICULO(
@@ -74,7 +101,7 @@ CREATE TABLE TIPO_DE_VEICULO(
     PRIMARY KEY (idCategoriaVeiculo, idCategoriaCarga),
     FOREIGN KEY (idCategoriaVeiculo) REFERENCES CATEGORIAS_VEICULO(id),
     FOREIGN KEY (idCategoriaCarga) REFERENCES CATEGORIAS_CARGA(id)
-)
+);
 
 -- Pertence: relação entre veiculo e tipo_de_veiculo
 
@@ -85,7 +112,7 @@ CREATE TABLE VEICULO_PERTENCE(
     PRIMARY KEY(idCategoriaVeiculo, idCategoriaCarga, placa),
     FOREIGN KEY(idCategoriaCarga, idCategoriaVeiculo) REFERENCES TIPO_DE_VEICULO(idCategoriaCarga, idCategoriaVeiculo),
     FOREIGN KEY(placa) REFERENCES VEICULOS(placa)
-)
+);
 
 -- Veiculos
 CREATE TABLE VEICULOS(
@@ -95,7 +122,7 @@ CREATE TABLE VEICULOS(
     numero_eixos INTEGER,
     idUnidadeTransportadora INTEGER,
     FOREIGN KEY (idUnidadeTransportadora) REFERENCES UNIDADES_TRANSPORTADORA(id)
-)
+);
 
 -- Viajem de entrega
 CREATE TABLE VIAJENS_ENTREGA(
@@ -106,7 +133,7 @@ CREATE TABLE VIAJENS_ENTREGA(
     placa CHAR(7),
     FOREIGN KEY (idMotorista) REFERENCES MOTORISTAS (id),
     FOREIGN KEY (placa) REFERENCES VEICULOS(placa)
-)
+);
 
 -- Lote de Entrega
 CREATE TABLE LOTES_ENTREGA(
@@ -117,7 +144,7 @@ CREATE TABLE LOTES_ENTREGA(
     FOREIGN KEY(idViajemEntrega) REFERENCES VIAJENS_ENTREGA(id),
     FOREIGN KEY(idFuncionarioAdministrativo) REFERENCES FUNCIONARIOS_ADMINISTRATIVOS(id),
     FOREIGN KEY(idOperario) REFERENCES OPERARIOS(id)
-)
+);
 
 -- Destino
 CREATE TABLE DESTINOS(
@@ -126,7 +153,7 @@ CREATE TABLE DESTINOS(
     estado CHAR(2),
     bairro VARCHAR(20),
     cidade VARCHAR(20),
-)
+);
 
 -- Produtos
 CREATE TABLE PRODUTOS(
@@ -137,7 +164,7 @@ CREATE TABLE PRODUTOS(
     cnpj CHAR(14),
     FOREIGN KEY (idLoteEntrega) REFERENCES LOTES_ENTREGA(id),
     FOREIGN KEY (cnpj) REFERENCES CLIENTES(cnpj)
-)
+);
 
 -- prepara lote
 
@@ -147,7 +174,7 @@ CREATE TABLE PREPARA_LOTE(
     PRIMARY KEY(idOperario, idLoteEntrega),
     FOREIGN KEY (idOperario) REFERENCES OPERARIOS(id),
     FOREIGN KEY (idLoteEntrega) REFERENCES LOTES_ENTREGA(id)
-)
+);
 
 -- cliente
 CREATE TABLE CLIENTES(
@@ -156,7 +183,7 @@ CREATE TABLE CLIENTES(
     telefone VARCHAR(15),
     destino INTEGER,
     FOREIGN KEY destino REFERENCES DESTINOS(id)
-)
+);
 
 -- Endereço Cliente
 CREATE TABLE ENDERECO_CLIENTE(
@@ -167,6 +194,6 @@ CREATE TABLE ENDERECO_CLIENTE(
     cnpjProprietario CHAR(14),
     PRIMARY KEY(numero, bairro, cidade, estado, cnpjProprietario),
     FOREIGN KEY (cnpjProprietario) REFERENCES CLIENTES(cnpj) ON DELETE CASCADE
-)
+);
 
 
